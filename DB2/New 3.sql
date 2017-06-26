6d5c4afd4eb39693d27e0b39b6de1fad614e0848
Brenda340 <brenda.hernadnez@ixpia.com.mx>
2017-06-26 09:49:42 -0500

    modificacion

--- DB2/New 3.sql
+++ DB2/New 3.sql
@@ -1,3 +1,32 @@
+--this is a comment
+REPLACE PROCEDURE C##ELI.Loopproc (INVAL NUMBER, N NUMBER)
+IS
+    TMPVAR    NUMBER;
+    TMPVAR2   NUMBER;
+    TOTAL     NUMBER;
+BEGIN
 
---this is a coment
+    TMPVAR := 0;
 
+    TMPVAR2 := 0;
+
+    TOTAL := 0;
+
+
+    FOR LCV IN 1 .. INVAL
+    LOOP
+        TOTAL := 2 * TOTAL + 1 - TMPVAR2;
+
+        TMPVAR2 := TMPVAR;
+
+        TMPVAR := TOTAL;
+
+    END LOOP;
+
+
+
+    DBMS_OUTPUT.PUT_LINE ('TOTAL IS' || TO_CHAR (TOTAL));
+
+END Loopproc;
+--this is a comment and
+--this is ither comment
