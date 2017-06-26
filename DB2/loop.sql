e17e4019ee216ea9347450c38975836ae0353c58
Brenda340 <brenda.hernadnez@ixpia.com.mx>
2017-06-26 09:34:20 -0500

    loop
    this is a loop

---
+++ DB2/loop.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE PROCEDURE C##ELI.Loopproc (INVAL NUMBER, N NUMBER)
+IS
+    TMPVAR    NUMBER;
+    TMPVAR2   NUMBER;
+    TOTAL     NUMBER;
+BEGIN
+
+    TMPVAR := 0;
+
+    TMPVAR2 := 0;
+
+    TOTAL := 0;
+
+
+    FOR LCV IN 1 .. INVAL
+    LOOP
+        TOTAL := 2 * TOTAL + 1 - TMPVAR2;
+
+        TMPVAR2 := TMPVAR;
+
+        TMPVAR := TOTAL;
+
+    END LOOP;
+
+
+
+    DBMS_OUTPUT.PUT_LINE ('TOTAL IS' || TO_CHAR (TOTAL));
+
+END Loopproc;
+--this is a comment and
